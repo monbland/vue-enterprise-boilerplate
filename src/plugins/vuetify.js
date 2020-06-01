f9d3e5301c90c0008a22acb379395b06a7eddd33
Andrey Mitkin <am@letsnova.ru>
2020-06-01 16:53:48 +0300

    fix: правильный импорт vuetify, как плагина

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
+const opts = {
+  theme: {
+    themes: {
+      primary: '#0037EE',
+    },
+  },
+}
+
+export default new Vuetify(opts)
